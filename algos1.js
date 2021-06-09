1545d44b4a3f43119bc26c237313e74a0fd9bd27
Joshua Speerbrecker <jhspeerbrecker@gmail.com>
2021-06-08 17:58:05 -0700

    Get 1 to 255

--- algos1.js
+++ algos1.js
@@ -1,21 +1,21 @@
 
-The default interactive shell is now zsh.
-To update your account to use zsh, please run `chsh -s /bin/zsh`.
-For more details, please visit https://support.apple.com/kb/HT208050.
-Jasons-iMac:Git Algos jasonspeerbrecker$ git add .
-Jasons-iMac:Git Algos jasonspeerbrecker$ git commit -a -m "first commit"
-[master (root-commit) f3d19ff] first commit
- 1 file changed, 1 insertion(+)
- create mode 100644 algos1.js
-Jasons-iMac:Git Algos jasonspeerbrecker$ git push origin masterEnumerating objects: 3, done.
-Counting objects: 100% (3/3), done.
-Writing objects: 100% (3/3), 221 bytes | 221.00 KiB/s, done.
-Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
-To https://github.com/Joshua-Speerbrecker/Git-Algo-Repo.git
- * [new branch]      master -> master
-Jasons-iMac:Git Algos jasonspeerbrecker$ 
+// The default interactive shell is now zsh.
+// To update your account to use zsh, please run `chsh -s /bin/zsh`.
+// For more details, please visit https://support.apple.com/kb/HT208050.
+// Jasons-iMac:Git Algos jasonspeerbrecker$ git add .
+// Jasons-iMac:Git Algos jasonspeerbrecker$ git commit -a -m "first commit"
+// [master (root-commit) f3d19ff] first commit
+//  1 file changed, 1 insertion(+)
+//  create mode 100644 algos1.js
+// Jasons-iMac:Git Algos jasonspeerbrecker$ git push origin masterEnumerating objects: 3, done.
+// Counting objects: 100% (3/3), done.
+// Writing objects: 100% (3/3), 221 bytes | 221.00 KiB/s, done.
+// Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+// To https://github.com/Joshua-Speerbrecker/Git-Algo-Repo.git
+//  * [new branch]      master -> master
+// Jasons-iMac:Git Algos jasonspeerbrecker$ 
 
 
 
-git add .
-git commit -a -m "good messa"
+// git add .
+// git commit -a -m "good messa"
