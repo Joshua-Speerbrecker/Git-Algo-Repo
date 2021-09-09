918f027f680d72f738304013e51184bbbbd0074e
Joshua Speerbrecker <jhspeerbrecker@gmail.com>
2021-09-09 16:23:27 -0700

    firstNonConsecutive

---
+++ firstNonConsecutive.js
@@ -0,0 +1,26 @@
+/* 
+  Given an array of integers, return the first non-consecutive integer
+    - a non-consecutive integer is one that is NOT exactly 1 larger
+      than the previous number
+  
+  Input: [1,2,3,4,6,7,8]
+  Output: 6
+  Input: [1, 2, 3]
+  Output: null
+*/
+
+const nums1 = [1,2,3]
+
+function firstNonConsecutive(nums) {
+    for (let i = 1; i < nums.length; i++) {
+      const prevNum = nums[i - 1];
+      const currNum = nums[i];
+  
+      if (prevNum + 1 !== currNum) {
+        return currNum;
+      }
+    }
+    return null;
+  }
+
+console.log(firstNonConsecutive(nums1))
